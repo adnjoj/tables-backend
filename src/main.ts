4461daeb8ae6550f42bdb54aed4b70ecd1c93121
adnjoj <morgoth123456798@gmail.com>
2022-05-28 08:14:52 +0800

    HttpException filter added to turn all errors in response to arrays

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import { NestFactory } from '@nestjs/core';
 import { useContainer } from 'class-validator';
 
 import { AppModule } from './app/app.module';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
 import './userRequest';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   app.useGlobalPipes(
     new ValidationPipe({
